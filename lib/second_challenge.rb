2e6b6352e16d4bfbdf1f7afb907d4386498e25c3
Luke Ojukwu <luke.ojukwu@gmail.com>
2019-02-06 17:22:54 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -8,7 +8,7 @@ def second_challenge
 
   #code your solution here!
 
-groceryArray = groceries.map{ |item| groceries.values
+groceryArray = groceries.map{ |item| groceries.values }.flatten
   
 
 end
