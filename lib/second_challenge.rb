5fbbb4db170d804dc43da94f5381283f653a4026
Luke Ojukwu <luke.ojukwu@gmail.com>
2019-02-06 17:15:27 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -8,13 +8,19 @@ def second_challenge
 
   #code your solution here!
 
+  groceryArray = []
 
-
+  groceries.each do |key, value|
+    
+    value.each do |attributes|
+      
+      attributes << groceryArray
+  
+    end
     
   end
   
 end
-
 
 
 end
