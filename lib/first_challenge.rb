adae08b29ebe0bfab02bc12524b3e4365e1a153d
Luke Ojukwu <luke.ojukwu@gmail.com>
2019-02-06 16:55:27 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -18,7 +18,7 @@ def first_challenge
     data.each do |attribute, value|
      
      if attribute == :favorite_icecream_flavors
-       value.each do |flavor|
+       value.delete_if do |flavor|
         end
         
         if flavor == "strawberry"
